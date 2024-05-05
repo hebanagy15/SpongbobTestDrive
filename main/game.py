ae346b1ae1e16ff53348998cf9c0ab9475394787
Slma2 <143320802+Slma2@users.noreply.github.com>
2024-05-05 20:32:33 +0300

    Update game.py

--- main/game.py
+++ main/game.py
@@ -1,15 +1,15 @@
 """
 Documentation:
-    1) Game store :
+    1) Game story:
          Spongebob Driving Test
          It is a game to help Spongebob to get the driver's license and consists of 3 stages,
          First one, which is the beginning of the game, a two-dimensional window to start or end.
-         The stage of the game, which is a three-dimensional window,
-         in which you must collect stars and not injure the fish and the diver.
-         The final stage consists of two cases winning or losing. 
-         If you win, you get the license and you exit.
-         or if you lose, you can start again or exit.
-         extra details:
+         Second one, which is a three-dimensional window,
+         in which you collect stars and not injure the fish and the diver.
+         The last, consists of two cases winning or losing. 
+         If you win, you get the license and exit.
+         If you lose, you start again or exit.
+         More details:
          When a fish is injured, the speed decreases, and when a star is token, the speed increases.
 
     2) about us :
@@ -19,15 +19,15 @@ Documentation:
     3- Salma Waleed Abdulshakoor
     4- Salma Hassan 
     5- Mariam Hassanin Mohammed
-    6- Fatma Nasser Atallah
+    6- Fatma Nasser Ataallah
     7- Menna Ehab Shoman
     8- Mona Muhammad Ayoub
     9- Heba Abdel Moneim Nagy
     10- Nourhan Sameh Fayyad
-    11- Habiba Mohammed Al Sharqawi
-    12- Mahmoud Kamal Alsadani
+    11- Habiba Mohammed El-Sharqawi
+    12- Mahmoud Kamal El-Sadani
 
-    3) Supervised by Dr/ Mohamed Ali and Eng/ Mohammed El koumy .
+    3) Supervised by Dr/ Mohamed Ali and Eng/ Mohammed El-koumy.
 
 """
 ######################################################################
